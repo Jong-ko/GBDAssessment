acea8d2d8a7db42023cd762c9ee0f8ca203cc478
Jeremy Ongko <ongkojeremy@gmail.com>
2024-01-15 15:39:07 -0500

    added logic for intersection observer instead of using scrollY method

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 console.log('boop')
 
-window.addEventListener('scroll', function() {
-  var floatingNav = document.getElementById('floatingNav');
-  if (window.scrollY > 110) {
-    console.log(`boop`)
-    floatingNav.classList.add('show');
-  } else {
-    console.log('beep')
-    floatingNav.classList.remove('show');
-  }
-});
+// window.addEventListener('scroll', function() {
+//   let floatingNav = document.getElementById('floatingNav');
+//   if (window.scrollY > 110) {
+//     console.log(`boop`)
+//     floatingNav.classList.add('show');
+//   } else {
+//     console.log('beep')
+//     floatingNav.classList.remove('show');
+//   }
+// });
+
+
+// Function to handle the intersection observer callback
+function handleIntersection(entries, observer) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      console.log(`remove`)
+      document.getElementById('floatingNav').classList.remove('show');
+    } else {
+      console.log(`show`)
+      document.getElementById('floatingNav').classList.add('show');
+    }
+  });
+}
+
+// Create an intersection observer
+let observer = new IntersectionObserver(handleIntersection, { threshold: 0 });
+
+// Observe the content element
+observer.observe(document.getElementById('headerNav'));
