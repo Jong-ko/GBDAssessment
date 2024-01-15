03da9fa748c0afabd11011f062596e7a65d695f5
Jeremy Ongko <ongkojeremy@gmail.com>
2024-01-15 16:17:38 -0500

    refactored logic

--- index.js
+++ index.js
@@ -3,24 +3,20 @@
 // window.addEventListener('scroll', function() {
 //   let floatingNav = document.getElementById('floatingNav');
 //   if (window.scrollY > 110) {
-//     console.log(`boop`)
+//     console.log(`show`)
 //     floatingNav.classList.add('show');
 //   } else {
-//     console.log('beep')
+//     console.log('remove')
 //     floatingNav.classList.remove('show');
 //   }
 // });
 
 // logic for what is triggered
 function handleIntersection(entries, observer) {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      // console.log(`remove`)
-      document.getElementById('floatingNav').classList.remove('show');
-    } else {
-      // console.log(`show`)
-      document.getElementById('floatingNav').classList.add('show');
-    }
+  entries.forEach((entry) => {
+    document
+      .getElementById("floatingNav")
+      .classList.toggle("show", !entry.isIntersecting);
   });
 }
 
@@ -28,4 +24,4 @@ function handleIntersection(entries, observer) {
 let observer = new IntersectionObserver(handleIntersection, { threshold: 0 });
 
 // sets headerNav to be observed
-observer.observe(document.getElementById('headerNav'));
+observer.observe(document.getElementById("headerNav"));
